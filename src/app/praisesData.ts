03e037abb00f00b2534d24cedb9be8a29e10a7dd
Gary Saunders <gary.saunders@hunterindustries.com>
2025-06-07 15:42:42 -0700

    ^ f - Adding assets

---
+++ src/app/praisesData.ts
@@ -0,0 +1,50 @@
+export const praisesData = [
+    {
+        'title': '',
+        'content':'"Gary Saunders is responsible for most all of the User Interface (UI) that amazes our Centralus customers every day. Gary regularly makes our job of developing the Centralus application so much easier. He works side by side with us daily to deliver a functional application with an aesthetically pleasing look and feel. Gary is approachable and available whenever we have questions or require assistance. I appreciate Gary\'s craftmanship and attention to detail that results in the fantastic fit and finish we see in Centralus. Thanks Gary!"',
+        'type':'recognition',
+        'person': 'Aaron',
+        'personTitle': 'Senior Software Engineer',
+        'position': 'left'
+    },
+    {
+        'title': '',
+        'content':'"Gotta say, it’s pretty exciting to download your changes and see how much better the app looks. Cool!"',
+        'type':'email',
+        'person': 'Colin',
+        'personTitle': 'Principle Software Engineer',
+        'position': 'right'
+    },
+    {
+        'title': '',
+        'content':'"Absolutely gorgeous design, Gary! We may want to discuss more with you the control to use for the Watering Mode in the Command Center mockup."',
+        'type':'teams',
+        'person': 'MobPilot1',
+        'personTitle': 'Software Engineering Mob',
+        'position': 'left'
+    },
+    {
+        'title': '',
+        'content':'"This is a nice win for our team. This customer actually converted to us because they liked our UI better, and it does all they need (for now). They loved the mobile interface and ease of LTE setup."',
+        'type':'email',
+        'person': 'Dave',
+        'personTitle': 'Senior Product Manager, Electronics',
+        'position': 'right'
+    },
+    {
+        'title': 'Complete Drip Calculator',
+        'content':'"I have downloaded a few of these calculators and this is the best one I have used. The ability to share the results is brilliant."',
+        'type':'appStore',
+        'person': 'Rockn\' All\' The\' Time',
+        'personTitle': 'iOS App Store User',
+        'position': 'left'
+    },
+    {
+        'title': '',
+        'content':'"Man! Really loving that UI design for the sensor page ~ great work seriously!!!"',
+        'type':'teams',
+        'person': 'Andy',
+        'personTitle': 'Senior Software Engineer',
+        'position': 'right'
+    },
+]
---
+++ src/app/projectsData.ts
@@ -0,0 +1,170 @@
+export const projectsData = [
+
+    // { 
+    //     'type': 'UX/UI Improvement',
+    //     'title': 'Modal Window',
+    //     'description': 'PROBLEM: Users were bypassing a critial step in the user flow when setting up a cellular-enabled IOT irrigation controller. SOLUTION: Adjust the messaging and action button labels to make it more clear what needs to occur and how to accomplish that action.',
+    //     'skills': [
+    //         "Adobe XD",
+    //         "HTML/SCSS/JS",
+    //         "Responding to feedback"
+    //     ],
+    //     'link': 'https://centralus.hunterindustries.com/',
+    //     'images': [
+    //         { 'src': '../../../assets/projects/zipit/zipit-modal-after.png'},
+    //         { 'src': '../../../assets/projects/zipit/zipit-modal-before.png'},
+    //     ],
+    //     'layout': 'imgLeft',
+    //     'screenshotType': 'desktop'
+    // },
+    { 
+        'type': 'Web App (DESIGN + DEVELOPMENT)',
+        'title': 'Centralus',
+        'description': 'Continually working with the software engineering teams to improve user interfaces and add new features in a specialized global IOT irrigation software called Centralus. Incorporating Angular Material to streamline and standardize the app UI while it is being migrated from AngularJS to Angular Next. Many iterative mockups were designed, prototypes were developed, and UI meetings were conducted over the course of many months to achieve successful rollouts of new features such as ACC2 scheduling, changing of device serial, and the cell module activation process.',
+        'skills': [
+            "Angular",
+            "Angular Material",
+            "Adobe XD"
+        ],
+        'link': 'https://centralus.hunterindustries.com/',
+        'images': [
+            { 'src': '../../../assets/projects/centralus-scheduling/project_centralus_1.png'},
+            { 'src': '../../../assets/projects/centralus-scheduling/project_centralus_2.png'},
+            { 'src': '../../../assets/projects/centralus-scheduling/project_centralus_3.png'},
+            { 'src': '../../../assets/projects/centralus-scheduling/project_centralus_4.png'},
+        ],
+        'layout': 'imgRight',
+        'screenshotType': 'mobile'
+    },
+    { 
+        'type': 'UX/UI Improvements (DESIGN + DEVELOPMENT)',
+        'title': 'Centralus Tech Support (Desktop)',
+        'description': 'A much needed UI update to a utility app used internally by the tech support team for troubleshooting IOT irrigation controllers. Improved UI design for desktop then developed with Angular / TypeScript / HTML / SCSS.',
+        'skills': [
+            "Adobe XD",
+            "Angular",
+            "Angular Material"
+        ],
+        'link': 'internal',
+        'images': [
+            { 'src': '../../../assets/projects/centralus-support/support-after.png'},
+            { 'src': '../../../assets/projects/centralus-support/support-before.png'},
+
+        ],
+        'layout': 'imgBottom',
+        'screenshotType': 'desktop'
+    },
+    { 
+        'type': 'App Feature (DESIGN)',
+        'title': 'Pilot CCS - POGO Integration',
+        'description': 'The team working on a golf course irrigation management software came to me with a need to integrate a new feature into the existing station edit side sheet. After coming to a mutual understanding of the desired functionality and visual display needed, I designed an appropriate solution and worked with the software engineers (mostly on the markup and styles) to implement the new feature which received much positive feedback from stakeholders and users.',
+        'skills': [
+            "Adobe XD",
+            "Angular",
+            "Angular Material",
+            "HTML",
+            "SCSS"
+        ],
+        'link': 'internal',
+        'images': [
+            { 'src': '../../../assets/projects/pilot/pogo-feature.png'},
+        ],
+        'layout': 'imgBottom',
+        'screenshotType': 'desktop'
+    },
+    { 
+        'type': 'Project (DESIGN)',
+        'title': 'Hunter Cellkit Mobile App',
+        'description': 'Worked with another vendor Digi to customize the Digi XBee Xamarin mobile app interface for a modified version needed to work with our IOT central irrigation software to bring the design more in line with the Hunter branding and similar apps in the Hunter family.',
+        'skills': [
+            "Adobe XD",
+            "Xamarin",
+            "Vendor Relations"
+        ],
+        'link': 'unreleased',
+        'images': [
+            { 'src': '../../../assets/projects/cellkit/screen1.png'},
+            { 'src': '../../../assets/projects/cellkit/screen2.png'},
+        ],
+        'layout': 'imgLeft',
+        'screenshotType': 'mobile'
+    },
+    { 
+        'type': 'Project (DESIGN + DEVELOPMENT)',
+        'title': 'Centralus Tech Support (Mobile)',
+        'description': 'A much needed UI update to a utility app used internally by the tech support team for troubleshooting IOT irrigation controllers. Created non-existent mobile design, then developed with Angular / TypeScript / HTML / SCSS.',
+        'skills': [
+            "Adobe XD",
+            "Angular",
+            "Angular Material"
+        ],
+        'link': 'internal',
+        'images': [
+            { 'src': '../../../assets/projects/centralus-support/project_centralus_support_3.png'},
+            { 'src': '../../../assets/projects/centralus-support/project_centralus_support_2.png'},
+            { 'src': '../../../assets/projects/centralus-support/project_centralus_support_1.png'},
+            { 'src': '../../../assets/projects/centralus-support/project_centralus_support_4.png'},
+        ],
+        'layout': 'imgRight',
+        'screenshotType': 'mobile'
+    },
+    { 
+        'type': 'Project (DESIGN + DEVELOPMENT)',
+        'title': 'Hunter Dripline Calculator Mobile App',
+        'description': 'Designed and built a hybrid mobile app using Vue.js and Cordova (PhoneGap). This was a utility app for users to input specifications for a drip irrigation project and have the app recommend products, calculate zone totals and/or calculate a tailored run time duration.',
+        'skills': [
+            "Vue.js",
+            "Cordova",
+            "Framework7",
+            "Sketch"
+        ],
+        'link': 'mobile-app',
+        'images': [
+            { 'src': '../../../assets/projects/dripcalc/screen1.png'},
+            { 'src': '../../../assets/projects/dripcalc/screen2.png'},
+            { 'src': '../../../assets/projects/dripcalc/screen3.png'},
+        ],
+        'layout': 'imgLeft',
+        'screenshotType': 'mobile'
+    },
+    { 
+        'type': 'Website (DEVELOPMENT)',
+        'title': 'Hunter Website',
+        'description': 'Main marketing website for the company that needed a modern facelift. Turned static mockups provided by the UX/UI designer into responsive and functional webpages. Performed custom Drupal module development and ongoing feature enhancements.',
+        'skills': [
+            "Drupal",
+            "HTML5",
+            "SCSS",
+            "PHP",
+            "Javascript/JQuery",
+            "Foundation"
+        ],
+        'link': 'https://www.hunterindustries.com/',
+        'images': [
+            { 'src': '../../../assets/projects/hunter-website/project_hunter_website.png'},
+            
+        ],
+        'layout': 'imgBottom',
+        'screenshotType': 'desktop'
+    },
+    { 
+        'type': 'Throwback Project (DESIGN + DEVELOPMENT)',
+        'title': '187 Killer Pads Customizer',
+        'description': 'A super fun side project I did a few years ago for a good friend. Users choose colors along with other options for each section of a skateboarding kneepad and at the end the user can click a button to purchase their creation. I designed and built the whole thing from scratch.',
+        'skills': [
+            "Sketch",
+            "HTML",
+            "CSS",
+            "JavaScript/jQuery"
+        ],
+        'link': 'https://187killerpads.com/products/custom-pad',
+        'images': [
+            { 'src': '../../../assets/projects/187/187-1.png'},
+            { 'src': '../../../assets/projects/187/187-2.png'},
+
+        ],
+        'layout': 'imgBottom',
+        'screenshotType': 'desktop'
+    },
+
+];
